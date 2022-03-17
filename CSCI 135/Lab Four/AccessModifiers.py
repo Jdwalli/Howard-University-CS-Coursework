ecdf4ba60cf11433cf16c89039089ddd9fdf812f
Joshua <Jdwalli2003@yahoo.com>
2022-03-17 11:49:12 -0400

    Completed Access Modifiers

--- CSCI 135/Lab Four/AccessModifiers.py
+++ CSCI 135/Lab Four/AccessModifiers.py
@@ -7,11 +7,11 @@ class AccessModifiers():
       # Private ( given private variable will be a list of integers (all elements are not guaranteed to be integers though) )
       self.__privateInstanceVariable = privateInstanceVariable
     
-
     def _strip_characters(self, element):
-        """Remove characters that do not matter
-        For both palindromes and anagrams punctuation and/or special characters 
-        and/or integers could be included BUT should be IGNORED
+        """
+            Remove characters that do not matter
+            For both palindromes and anagrams punctuation and/or special characters 
+            and/or integers could be included BUT should be IGNORED
         """
         clean_string = ""
         for character in element.lower():
@@ -20,6 +20,9 @@ class AccessModifiers():
         return clean_string
 
     def _is_palindrome(self, word):
+      if word == "":
+        return False
+      else:
         return word == word[::-1]
 
     def _create_frequency_dict(self, string):
@@ -28,7 +31,6 @@ class AccessModifiers():
             freq[char] += 1
         return freq
 
-    
     def _is_anagram(self, element1, element2, k):
         freq1 = self._create_frequency_dict(element1)
         freq2 = self._create_frequency_dict(element2)
@@ -38,7 +40,6 @@ class AccessModifiers():
         else:
             pass
 
-    
     def __privateInstanceMethod(self) -> int:
         odd_add = True
         even_add = False
@@ -62,27 +63,20 @@ class AccessModifiers():
                         odd_add = True
         return even_sum * odd_sum
 
-
     def publicInstanceMethod(self) -> tuple:
         valid_palindromes = []
+        valid_anagrams = []
+        
         for index, element in enumerate(self.publicInstanceVariable):
             if isinstance(element, str):
                 element = self._strip_characters(element)
                 if self._is_palindrome(element):
                     valid_palindromes.append(self.publicInstanceVariable[index]) 
         
-        valid_anagrams = []
         for index, element in enumerate(self._protectedInstanceVariable):
             word1, word2, k = element
-            word1 = self._strip_characters(word1)
-            word2 = self._strip_characters(word2)
-            if self._is_anagram(word1, word2, k):
+            if len(self._strip_characters(word1)) - k == len(self._strip_characters(word2)):
+                valid_anagrams.append(element)
+            if len(self._strip_characters(word2)) - k == len(self._strip_characters(word1)):
                 valid_anagrams.append(element)
         return (valid_palindromes, valid_anagrams, self.__privateInstanceMethod())
-
-
-if __name__ == "__main__":
-    publicInstanceVariable = ['determine', 26, 'mom0', 'articulate', 74]
-    protectedInstanceVariable = [('Leonardo asw Vinci', 'O, Draconhsyebjian devil!', 6), ('Oh, lame sajwyint!', 'The Mona Lisa', 11), ('Tribainia', 'Brijtain', 3), ('Langyeeeypqbden', 'Engsieybfland', 8), ('Madonna of thjeyye Rocks', 'So dark the cncheon of Man', 9)]
-    privateInstanceVariable = [39, 'glance', 35, 19, 49]
-    assert (AccessModifiers(publicInstanceVariable, protectedInstanceVariable, privateInstanceVariable).publicInstanceMethod()) == (['mom0'], [], 0)

e29b8fac40806fa0a84c313a8f41c71f704d2f6a
Joshua <Jdwalli2003@yahoo.com>
2022-04-10 17:05:31 -0400

    Created method for encrypting and printing

--- Unix Lab/cipher.py
+++ Unix Lab/cipher.py
@@ -1,8 +1,28 @@
 import sys 
 
+def encrypt(message, key):
+    # Discard all the punctuation marks, digits, blanks, and anything else from the input string.
+    EXCLUSION_STRING = "123456789!@#$%^&*()+_~[{]}:;',<.>?/ "
+    encrypted_message = ""
 
-def encrypt(key):
-    pass
+    for character in message:
+        if character not in EXCLUSION_STRING:
+            encrypted_message += chr(( ord(character) + key - 65) % 26 + 65)
+    return encrypted_message
+
+def print_encrypted_message(encoded_message):
+    line_count = 0
+    for index, char in enumerate(encoded_message):
+        if line_count == 10:
+            print("")
+            line_count = 0
+        print(char, end="")
+        if index % 5 == 4:
+            print(" ", end="")
+            line_count += 1
+    print("")
+   
+    
 
 
 if __name__ == "__main__":
@@ -18,4 +38,4 @@ if __name__ == "__main__":
     message = input("Enter your unencoded message: ")
     # Convert the message to all uppercase.
     message = message.upper()
-    
+    print_encrypted_message(encrypt(message, key))

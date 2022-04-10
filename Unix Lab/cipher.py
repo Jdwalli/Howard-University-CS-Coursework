04678624dcaa81fb8cc6397ef36ba8226879b66d
Joshua <Jdwalli2003@yahoo.com>
2022-04-10 16:41:32 -0400

    Gathered input under the main method!

--- Unix Lab/cipher.py
+++ Unix Lab/cipher.py
@@ -6,4 +6,16 @@ def encrypt(key):
 
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) == 1:
+        print("Please add a key argument!")
+    elif len(sys.argv) == 2:
+        try:
+            key = int(sys.argv[1])
+        except Exception:
+            print("This was not a valid key! Try again with a number!")
+    else:
+        print("Please only add one key argument!")
+    message = input("Enter your unencoded message: ")
+    # Convert the message to all uppercase.
+    message = message.upper()
+    

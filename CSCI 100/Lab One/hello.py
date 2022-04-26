e11302ad613764e71ebf7cbf4d2b78b4a4d37bb0
Joshua <Jdwalli2003@yahoo.com>
2022-04-25 23:02:56 -0400

    Uploaded lab one code

---
+++ CSCI 100/Lab One/hello.py
@@ -0,0 +1,6 @@
+#Use input() function to take a user input and assign the user's input to a variable 
+
+name = input("What is your name? ")
+
+# print the value of the variable using print() function 
+print("Hello, " + name)
